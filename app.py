15ec76eb8eb91c578e993a6e9d982a05401016a3
GANESH VIJAY KADAM <161746902+student-ganesh@users.noreply.github.com>
2025-05-14 21:21:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 groq_api_key =os.getenv("GROQ_API_KEY")
 
